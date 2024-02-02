f7ae959a34482b7f27943cd8de38238960323d89
ykcnngl <120281995+ykcnngl@users.noreply.github.com>
2024-02-02 11:43:23 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 0; a <= 11; a++)
+            {
+                Console.WriteLine("UnityDersleri");
+            }
+            Console.ReadLine();
+        }
+    }
+}
